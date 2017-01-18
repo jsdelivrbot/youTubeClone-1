f923cb739647b7041c15448ff1f7831f17ab8137
mlombard82 <mlombard82@gmail.com>
2017-01-18 09:33:01 -0500

    All the basic setup up to Refactoring the App component from functional to class-based

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+ import React, { Component } from 'react'; 
+ import ReactDOM from 'react-dom';
+ import YTSearch from 'youtube-api-search'; 
+ import SearchBar from './components/search_bar';
+ const API_KEY = 'AIzaSyDjS3f6lSKsWhwwwRzHCkpFupnLomXDQmU';
 
-import App from './components/app';
-import reducers from './reducers';
+ // Create a new component. This component should produce
+ // some HTML
+class App extends Component {
+  constructor(props){
+    super(props);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    this.state = { videos: [] };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+      YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.setState({ videos });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+} 
+ 
+ // Take this component's generated HTML and put it 
+ // on the page (DOM)
+
+ ReactDOM.render(<App />, document.querySelector('.container'));
